e0118653ac5472737619f51d0774341d338c11b7
HENRICHSENAG <98524921+HENRICHSENAG@users.noreply.github.com>
2022-04-14 10:32:55 +0200

    Update Auswerten.js

--- JS/Auswerten.js
+++ JS/Auswerten.js
@@ -60,7 +60,7 @@ function sendEmail( name, unternehmen, ticket, bearbeiter, anrede, Bearbeitungsz
     Host: "smtp.gmail.com",
     Username: "noreplayhen@gmail.com",
     Password: "Email123!",
-    To: mail /*+ ", support@henrichsen.de, jeannette.bastian@henrichsen.de, christian.sagmeister@henrichsen.de, andre.baltheiser@henrichsen.de" */,
+    To: mail + ", support@henrichsen.de, jeannette.bastian@henrichsen.de, christian.sagmeister@henrichsen.de, andre.baltheiser@henrichsen.de",
     From: "noreplayhen@gmail.com",
     Subject: ticket + " wurde bewertet",
     Body:  " Alle Daten: <br><br> Name: " + anrede + " " + name + " <br> Unternehmen: " + unternehmen + "<br> Ticketnummer: " + ticket + "<br> Bearbeiter: " + bearbeiter + "<br> <br> Ergebnis: <br> <br> " + 
@@ -77,15 +77,13 @@ function sendEmail( name, unternehmen, ticket, bearbeiter, anrede, Bearbeitungsz
          
             
    })
-alert ("Email send to " + mail);
    
 }  
 
 
 
 function createLink(id, Sterne) {
-    /*var Link ="https://henrichsen.topdesk.net/solutions/five-star-review-1/process/" + id + "/" + Sterne + "/TcmtnqxCxR_EfAeEyw2K53ypMz_23UhCa2ZRq2_QF62qb1VqEiVGqS9mqGJPI_5T9wWlf2YSoUC_ywhLuiTNBg";
-    window.open(Link);*/
-    alert ("Insert Topdesk");
+    var Link ="https://henrichsen.topdesk.net/solutions/five-star-review-1/process/" + id + "/" + Sterne + "/TcmtnqxCxR_EfAeEyw2K53ypMz_23UhCa2ZRq2_QF62qb1VqEiVGqS9mqGJPI_5T9wWlf2YSoUC_ywhLuiTNBg";
+    window.open(Link);
 }
 
